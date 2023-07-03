e879ba1d65eba210d2272f1ab7a45bb7b6560e03
Maayn-Luzon <d46880@ganyavne.ort.org.il>
2023-07-03 16:20:46 +0300

    changes im margin, prints and the direction of speed

--- robot2022/src/main/java/frc/robot/actions/RotateAngle.java
+++ robot2022/src/main/java/frc/robot/actions/RotateAngle.java
@@ -5,11 +5,12 @@ import com.flash3388.flashlib.scheduling.ActionControl;
 import com.flash3388.flashlib.scheduling.FinishReason;
 import com.flash3388.flashlib.scheduling.actions.ActionBase;
 import com.jmath.ExtendedMath;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Swerve;
 
 public class RotateAngle extends ActionBase {
 
-    private static final double MARGIN = 3.0;
+    private static final double MARGIN = 5;
 
     private final Swerve swerve;
     private final double rotateByDegrees;
@@ -38,12 +39,13 @@ public class RotateAngle extends ActionBase {
     @Override
     public void execute(ActionControl actionControl) {
         double rotation = (destinationDegrees - swerve.getHeadingDegrees()) / rotateByDegrees * rotateDirection.sign();
-        swerve.drive(0, 0, rotation * Swerve.MAX_SPEED);
-
+        swerve.drive(0, 0, -rotation * Swerve.MAX_SPEED);
+        SmartDashboard.putBoolean("YES", false);
         if (ExtendedMath.constrained(
                 swerve.getHeadingDegrees(),
                 destinationDegrees - MARGIN,
                 destinationDegrees + MARGIN)) {
+            SmartDashboard.putBoolean("YES", true);
             actionControl.finish();
         }
     }
