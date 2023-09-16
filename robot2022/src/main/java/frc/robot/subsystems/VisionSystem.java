c941b5b13b07346c2aa7bf8a0c22b0456926290d
Maayan <d46880@ganyavne.ort.org.il>
2023-09-16 19:00:37 +0300

    Distance equation

--- robot2022/src/main/java/frc/robot/subsystems/VisionSystem.java
+++ robot2022/src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.flash3388.flashlib.scheduling.Subsystem;
+import edu.wpi.first.math.util.Units;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
@@ -11,7 +12,14 @@ public class VisionSystem extends Subsystem {
     private PhotonCamera camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
 
     private static final double CONE_HEIGHT = 00; //**
-    private static final double FOCAL_LENGTH = 00;//**
+    private static final double CAMERA_HEIGHT = 00;//**
+
+    // you need to find it by creating the equation with known constants:
+    // place the cone in a known distance and height. know the camera's height as well.
+    // using the photon-vision website, find the pitch of the target and then solve the next equation ->
+    // tan(a1+a2) = (h2-h1) / d   you need to find "a1"
+    // forward explanation at https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+    final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
 
     public VisionSystem() {
@@ -33,9 +41,15 @@ public class VisionSystem extends Subsystem {
         PhotonPipelineResult pipelineResult = camera.getLatestResult();
         if(!pipelineResult.hasTargets())
             return 0;
+
         PhotonTrackedTarget bestTarget = pipelineResult.getBestTarget();
-      //  return (FOCAL_LENGTH*CONE_HEIGHT)/bestTarget.
-      //  PhotonUtils.calculateDistanceToTargetMeters()
-        return 0;
+
+        // to know more about the next function press ctrl+b on this function, and you will found out
+        // that the equation is similar to the one above
+        return PhotonUtils.calculateDistanceToTargetMeters(
+                CAMERA_HEIGHT,
+                CONE_HEIGHT,
+                CAMERA_PITCH_RADIANS,
+                Units.degreesToRadians(bestTarget.getPitch()));
     }
 }
